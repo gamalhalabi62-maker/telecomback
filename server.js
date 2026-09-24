8b7b4d20c44fe24b3b629303d72ef1be0f824ba4
zestt2024-dot <zestt2024@gmail.com>
2026-09-24 07:23:16 +0300

    Add Cloudinary for image/video storagwheks

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 });
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
